a8d54441dadc6ab4f1453f74abd6050a3799f3a7
FASTSHIFT <1290176185@qq.com>
2019-02-18 11:45:55 +0800

    更正脚本

--- Script(for FFmpeg)/FFmpegConvent.cmd
+++ Script(for FFmpeg)/FFmpegConvent.cmd
@@ -2,13 +2,13 @@
 F:
 cd F:\FFmpeg
 echo 正在转换分辨率...
-ffmpeg -i F:\HOP.flv -vf scale=128:64 -ss 00:00:00 -t 00:03:19 F:\HOP128x64.mkv
+ffmpeg -i F:\BadApple.mp4 -vf scale=128:64 -ss 00:00:00 -t 00:03:39 F:\BadApple128x64.mkv
 echo 分辨率转换结束
 pause
 
 echo 正在创建目录...
-md F:\HOPBmp
+md F:\BadAppleBmp
 echo 正在转换图片...
-ffmpeg -i F:\HOP128x64.mkv -vf hflip -r 51.45 -ss 00:00:00 -t 00:03:19 F:\HOPBmp\%%d.bmp
+ffmpeg -i F:\BadApple128x64.mkv -vf hflip -r 37.68 -ss 00:00:00 -t 00:03:39 F:\BadAppleBmp\%%d.bmp
 echo 图片转换结束
 pause
